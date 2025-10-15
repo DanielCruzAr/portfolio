2c1d890cfce26b1446033b922cf21eb329caebc3
Daniel Cruz <daniel@chamb.app>
2025-10-14 21:08:14 -0600

    earth's sphere and countries json

---
+++ src/app/[locale]/map/page.tsx
@@ -0,0 +1,12 @@
+import ThreeScene from '@/components/ThreeScene'
+import React from 'react'
+
+function Map() {
+  return (
+    <div className='flex justify-center items-center h-screen w-screen'>
+      <ThreeScene />
+    </div>
+  )
+}
+
+export default Map
